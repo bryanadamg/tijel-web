0e66bc581398c1456abbc9f4e61436b741add11c
bryanadamg <bryanadamg@datasintesa.id>
2023-04-16 00:29:27 +0700

    first template

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,10 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, third commit in git'
+    return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True, port=3000, host='0.0.0.0')
